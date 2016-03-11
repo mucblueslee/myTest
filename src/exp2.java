e9647fa231f3e0631bb7adb2507ee2d203e59c0c
mucblueslee <18811303201@163.com>
2016-03-11 10:30:45 +0800

    1234

---
+++ src/exp2.java
@@ -0,0 +1,11 @@
+public class exp2{
+	public static void main(String args){
+		int i=0;
+		math mymath=new math();
+		for(i=2;i<200;i++)
+			if(mymath.iszhishu(i)==true)
+				System.out.println(i);
+	}
+}
+
+
